048982455a847b13c460abab6793c306eefb8b8d
Johannes Bähr <b_and_j_gamers@gmx.de>
2022-10-30 17:37:03 +0100

    My answers to the exercises

--- src/exercises/express-exercises.js
+++ src/exercises/express-exercises.js
@@ -1,9 +1,16 @@
 import { Router } from 'express'
 // import {v4 as uuid} from "uuid";
-// import db from '../db.js'
+import db from '../db.js'
 
-const router = Router()
-// const { Users, Transfers } = db
+const app = Router() // ich hab es app genannt, in index.js ist es auch app
+
+const { Users, Transfers } = db
+
+// requests immer mit /exercises am anfang
+
+app.get('/users/:userId/books/:bookId', (req, res) => {
+    res.json(req.params)
+})
 
 // Express routing guide
 // https://expressjs.com/en/guide/routing.html
@@ -15,6 +22,10 @@ const router = Router()
 // the URL should be http://localhost:3000/exercises/express/user/<userId>
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find
 
+app.get('/user_db/:num', (req, res) => {
+    console.log(Users[req.params['num']])
+    res.json(Users[req.params['num']] !== undefined ? Users[req.params['num']] : "500: Internal Server error")
+})
 
 // Express-02
 // Build an endpoint that creates a new user in the db and returns the new total count of users in the db
@@ -22,12 +33,34 @@ const router = Router()
 // https://www.restapitutorial.com/lessons/httpmethods.html
 // https://developer.mozilla.org/en-US/docs/web/javascript/reference/global_objects/array/push
 
+app.post('/add_user', (req, res) => {
+    Users.push(req.body)
+    console.log(req.body)
+    res.json(Users)
+})
 
 // Express-03
 // Build an endpoint that allows user A to make a transaction to user B
 // The request body should look like that: {"from":"0123","to":"3210","amount":10}
 // https://javascript.info/object-copy
-// https://www.npmjs.com/package/uuid
+// https://www.npmjs.com/package/uuid | wofür ist das? und was soll ich mit dem array "Transfers" machen
+// TODO: How does the URL in Postman look?
 
+app.post('/express/transfer', (req, res) => {
+    let body = req.body
 
-export default router
+    let from_user = Users.find(obj => {
+        return obj.userId === body.from
+    })
+
+    let to_user = Users.find(obj => {
+        return obj.userId === body.to
+    })
+
+    to_user.balance += body.amount
+    from_user.balance -= body.amount
+
+    res.send(Users)
+})
+
+export default app
--- src/exercises/node-exercises.js
+++ src/exercises/node-exercises.js
@@ -2,8 +2,10 @@ import { Router } from 'express'
 
 const router = Router()
 
+// TODO: Node exercises ohne express, dann sind die express speziefischen statements wie z.B. "router.get" verwirrend.
+
 // Java Script basics crash course
-// https://developer.mozilla.org/en-US/docs/Learn/Getting_started_with_the_web/JavaScript_basics#language_basics_crash_course
+// https://developer.mozilla.org/en-US/docs/Learn/Getting_started_with_the_web/JavaScript_basics#language_basics_crash_course | Im Crash-Course geht es um JavaScript im Browser, vieleicht Fragen sich manche wo ein Knopf sein soll, der in dem Crash-Course genannt wurde
 
 // The exercises are inspired by:
 // https://javascript.info/
@@ -12,27 +14,35 @@ const router = Router()
 // Console output
 // https://developer.mozilla.org/en-US/docs/Web/API/Console/log
 function exercise01(inputString) {
-
+  console.log(inputString)
+  console.error(inputString)
 }
-router.get('/node-01/:functionInput', (req, res) => {
-  const functionInput = req.params?.functionInput
+router.get('/node-01/', (req, res) => {
+  const functionInput = req.query.id
   exercise01(functionInput)
-  res.json({exercise: 'Log the input in the console'})
+  res.json({result : req.query.id})
 })
 
 // Data Types
 // https://javascript.info/types#type-typeof
 function exercise02(inputString) {
+  console.log(typeof inputString)
 
+  try {
+    console.log(Number(inputString))
+  }
+  catch (e)  {
+    console.log(e.error)
+  }
+  return typeof inputString
 }
 router.get('/node-02/:functionInput', (req, res) => {
   const functionInput = req.params?.functionInput
-  exercise02(functionInput)
-  res.json({exercise: 'Print type of input to console'})
+  res.json({res : exercise02(functionInput)})
 })
 
 function exercise03(inputString) {
-
+  return typeof inputString
 }
 router.get('/node-03/:functionInput', (req, res) => {
   const functionInput = req.params?.functionInput
@@ -44,7 +54,11 @@ router.get('/node-03/:functionInput', (req, res) => {
 // Conversion
 // https://javascript.info/type-conversions#numeric-conversion
 function exercise04(inputString) {
-
+  if (isNaN(inputString)) {
+    return "Input is not a string"
+  } else {
+    return inputString * 2
+  }
 }
 router.get('/node-04/:functionInput', (req, res) => {
   const functionInput = req.params?.functionInput
@@ -56,7 +70,11 @@ router.get('/node-04/:functionInput', (req, res) => {
 // Comparisons
 // https://javascript.info/comparison
 function exercise05(inputString) {
-
+  if (!isNaN(inputString) && inputString > 10) {
+    return "true"
+  } else {
+    return "false"
+  }
 }
 router.get('/node-05/:functionInput', (req, res) => {
   const functionInput = req.params?.functionInput
@@ -68,7 +86,7 @@ router.get('/node-05/:functionInput', (req, res) => {
 // Objects
 // https://javascript.info/object
 function exercise06(inputObject) {
-
+  return inputObject.lastName
 }
 router.post('/node-06', (req, res) => {
   const functionInput = req.body // {"firstName": "John", "lastName": "Smith"}
@@ -76,9 +94,13 @@ router.post('/node-06', (req, res) => {
   res.json({exercise: 'Return the last name of the user', result: answer})
 })
 
+
 // https://javascript.info/object#square-brackets
 function exercise07(inputObject) {
-
+  let new_obj = {}
+  new_obj['propertyName'] = inputObject.propertyName
+  new_obj['propertyValue'] = inputObject.propertyValue
+  return new_obj
 }
 router.post('/node-07', (req, res) => {
   const functionInput = req.body  // {"propertyName": "isAdmin", "propertyValue": true}
@@ -91,7 +113,7 @@ router.post('/node-07', (req, res) => {
 // https://javascript.info/array
 // https://javascript.info/array#loops
 function exercise08(inputArray) {
-
+  return inputArray.join(" ")
 }
 router.post('/node-08', (req, res) => {
   const functionInput = req.body  // ["This", "is", "an", "array!"]
@@ -101,8 +123,9 @@ router.post('/node-08', (req, res) => {
 
 // https://javascript.info/string#string-length
 // https://javascript.info/array-methods#transform-an-array
+// TODO: What is a loop -> Documentation link
 function exercise09(inputArray) {
-
+  return inputArray.join("").length
 }
 router.post('/node-09', (req, res) => {
   const functionInput = req.body  // ["This", "is", "an", "array!"]
